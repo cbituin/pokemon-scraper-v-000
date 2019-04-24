4987924d32ccb66854d6f045cc684b0caa803894
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-04-24 04:02:37 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,11 +15,10 @@ class Pokemon
   def self.find(id, db)
     sql = db.prepare("SELECT * FROM ? WHERE id=?")
 
-    String.each do |s|
-      db.execute(s).each do |row|
+      db.execute(sql).each do |row|
         Pokemon.new(row[0], row[1], row[2], db)
       end
-    end
+
   end
 
 end
