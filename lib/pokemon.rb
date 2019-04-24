636bb6ebc43cae9a380c291a60c4f8801e2e0b63
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-04-24 03:39:30 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,6 @@
 class Pokemon
 
+<<<<<<< HEAD
   attr_accessor :id, :name, :type, :db
 
   def initialize(id: nil, name:, type:, db:)
@@ -18,6 +19,22 @@ class Pokemon
     db.execute(sql, db, id).each do |row|
       Pokemon.new(row[0], row[1], row[2], db)
     end.first
+=======
+  attr_accessor :id, :name, :type
+
+  def initialize(id=nil, name, type)
+    @id = id
+    @name = name
+    @type = type
+  end
+
+  def self.save(row)
+    # sql = "INSERT INTO "
+  end
+
+  def self.find
+
+>>>>>>> 5b2880d9361bea309f2b787d9b88ae30b21681a5
   end
 
 end
